9fd49acb4c6acfa53233625195e955f77126296d
Francisco <francisco.olsilva.0307@gmail.com>
2025-06-28 18:58:56 -0300

    Remove div desnecessária do componente App e adiciona animação de vídeo no componente Hero, com suporte para dispositivos móveis. Refatora o layout e a estrutura do componente Hero para melhor organização e responsividade.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,14 @@
 import { useGSAP } from "@gsap/react"
 import { SplitText } from "gsap/all"
 import gsap from "gsap"
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 function Hero() {
+
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    const isMobile = useMediaQuery({ maxWidth: 767 });
 
     useGSAP(() => {
         const heroSplit = new SplitText(".title", {
@@ -38,41 +44,71 @@ function Hero() {
                 scrub: true,
             }
         })
-        .to(".left-leaf", { y:-200 }, 0)
-        .to(".right-leaf", { y:200 }, 0)
-        
+            .to(".left-leaf", { y: -200 }, 0)
+            .to(".right-leaf", { y: 200 }, 0)
+
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
+
+        // Timeline para animar o vídeo durante o scroll
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "video",
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            }
+        })
+        if (videoRef.current) {
+            videoRef.current.onloadedmetadata = () => {
+                tl.to(videoRef.current, {
+                    currentTime: videoRef.current?.duration
+                })
+            }
+        }
     }, [])
-  return (
-    <>
-        <section id="hero" className='noisy'>
-            <h1 className='title'> Carlito</h1>
-            <img src="/images/hero-left-leaf.png" alt="left-leaf" className='left-leaf' />
-            <img src="/images/hero-right-leaf.png" alt="right-leaf" className='right-leaf' />
+    return (
+        <>
+            <section id="hero" className='noisy'>
+                <h1 className='title'> Carlito</h1>
+                <img src="/images/hero-left-leaf.png" alt="left-leaf" className='left-leaf' />
+                <img src="/images/hero-right-leaf.png" alt="right-leaf" className='right-leaf' />
 
-            <div className="body">
-                <div className="content">
-                    <div className='space-y-5 hidden md:block'>
-                        <p>Sapeca. Crocante. Classico.</p>
-                        <p className='subtitle'>
-                            Sinta o sabor <br/> da Japonesa
-                        </p>
-                    </div>
+                <div className="body">
+                    <div className="content">
+                        <div className='space-y-5 hidden md:block'>
+                            <p>Sapeca. Crocante. Classico.</p>
+                            <p className='subtitle'>
+                                Sinta o sabor <br /> da Japonesa
+                            </p>
+                        </div>
 
-                    <div className='view-cocktails'>
-                        <p className='subtitle'>
-                            Todo cocktail do nosso menu é
-                            uma história de sonhos distantes
-                            e aventuras que ainda não aconteceram
-                        </p>
-                        <a href="#cocktails">
-                            <p>Ver nossos cocktails</p>
-                        </a>
+                        <div className='view-cocktails'>
+                            <p className='subtitle'>
+                                Todo cocktail do nosso menu é
+                                uma história de sonhos distantes
+                                e aventuras que ainda não aconteceram
+                            </p>
+                            <a href="#cocktails">
+                                <p>Ver nossos cocktails</p>
+                            </a>
+                        </div>
                     </div>
                 </div>
+            </section>
+
+            <div className="video absolute inset-0">
+                <video 
+                src="/videos/output.mp4" 
+                ref={videoRef} 
+                muted 
+                playsInline 
+                preload="auto" 
+                className="w-full h-full object-cover" />
             </div>
-        </section>
-    </>
-  )
+        </>
+    )
 }
 
 export default Hero
